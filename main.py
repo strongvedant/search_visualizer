edfbc71d0c6ff322b92b1b2be680787121dad8c5
Vedant remella <remellavedant71@gmail.com>
2026-03-14 14:38:52 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import matplotlib
+import numpy
+print(numpy.__version__)
+print(matplotlib.__version__)
+
+maze = [
+    ['S', '.', '.', '#', '.'],
+    ['#', '.', '.', '#', '.'],
+    ['.', '.', '.', '.', '.'],
+    ['.', '#', '#', '.', '.'],
+    ['.', '.', '.', '.', 'G']
+]
+print(maze)
+1
